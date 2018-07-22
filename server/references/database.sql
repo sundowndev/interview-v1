37904719fae9107691b5fdfa82f2e6263b961a3d
sundowndev <raphael@crvx.fr>
2018-07-22 02:34:43 +0200

    Database structure dump

---
+++ server/references/database.sql
@@ -0,0 +1,44 @@
+-- Adminer 4.6.3 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP DATABASE IF EXISTS `app1`;
+CREATE DATABASE `app1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `app1`;
+
+DROP TABLE IF EXISTS `Session`;
+CREATE TABLE `Session` (
+  `user_id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `issued_at` datetime NOT NULL,
+  `expire_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `Task`;
+CREATE TABLE `Task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Task_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2018-07-22 00:34:19
